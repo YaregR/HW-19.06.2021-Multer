844c64382bc90eec471ffde72439dc928e49f054
Yareg <yaregr@gmail.com>
2021-06-29 21:10:39 +0300

    second level

--- public/js/main_html.js
+++ public/js/main_html.js
@@ -3,5 +3,5 @@ formEl.addEventListener('submit', async (ev) => {
     ev.preventDefault();
     const formData = new FormData(ev.target);
     const { data } = await axios.post('/', formData);
-    console.log('ответ', data);
+    console.log('ответ сервера:', data);
 });
--- routes/main.js
+++ routes/main.js
@@ -2,18 +2,42 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 const upload = multer({ 
     dest: path.join(__dirname, '../public/uploads/')
 });
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null, './public/uploads/');
+//     },
+//     filename: (req, file, cb) => {
+//       cb(null, file.originalname);
+//     }
+//   });
+  
+// const upload = multer({ storage: storage });
 
 router.get('/', (req, res) => {
     res.render('html');
 });
 
 router.post('/', upload.single('myFile'), (req, res) => {
-    console.log(req.file);
-    res.json({ status: 'ok' });
+     
+    let filedata = req.file;
+    if(!filedata)
+        res.send("Ошибка при загрузке файла");
+    else
+        res.send("Файл загружен");
+
+    const filename = path.join(__dirname, '../public/uploads/', req.file.filename);
+    const myId = path.join(__dirname, '../public/uploads/', req.body.myId);
+
+    const newName = fs.rename(filename, myId, () => {
+        console.log('Файл переименован!');
+    });
+
 });
 
 module.exports = router;
